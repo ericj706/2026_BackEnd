233e77b216938c1bf5f9cb2d4e2d978409be85b1
Eric <ericj6215@gmail.com>
2026-08-05 15:05:31 +0900

    day06_15:00

--- src/day06/Test.java
+++ src/day06/Test.java
@@ -3,60 +3,50 @@ package day06;
 import java.util.Scanner;
 
 public class Test {
-    public class OverallController {
-
-    Post[] posts = new Post[100];
-
     public static void main(String[] args) {
-        OverallController controller = new OverallController();
         Scanner scan = new Scanner(System.in);
+        Post[] pArry = new Post[100];
 
-        for (;;) {
-            System.out.println("============ My Community ============ ");
-            System.out.println("1.게시물쓰기 2.게시물출력");
-            System.out.println("====================================== ");
-            System.out.print("선택>");
-            int ch = scan.nextInt();
+        for(;;){
+            System.out.println("============Community============");
+            System.out.println("1. 글쓰기 | 2. 게시글 보기");
+            System.out.println("===============================");
+            System.out.print("선택> "); int ch = scan.nextInt();
 
-            if (ch == 1) {
+            if (ch==1) {
                 scan.nextLine();
-                System.out.print("내용 : ");
-                String content = scan.nextLine();
-                System.out.print("작성자 : ");
-                String writer = scan.nextLine();
+                System.out.print("내용: "); String content = scan.nextLine();
+                System.out.print("작성자: "); String writer = scan.nextLine();
 
                 Post post = new Post(content, writer);
                 boolean result = false;
 
-                for (int index = 0; index <= controller.posts.length - 1; index++) {
-                    if (controller.posts[index] == null) {
-                        controller.posts[index] = post;
+                for (int i = 0; i < pArry.length; i++) {
+                    if (pArry[i] == null) {
+                        pArry[i] = post;
                         result = true;
                         break;
                     }
                 }
+                if (result == true) {
+                    System.out.println("작성 완료");
+                }else{System.out.println("작성 실패");}
 
-                if (result) {
-                    System.out.println("[안내] 글쓰기 성공");
-                } else {
-                    System.out.println("[안내] 글쓰기 실패");
-                }
-
-            } else if (ch == 2) {
-                for (Post post : controller.posts) {
-                    if (post != null) {
-                        System.out.printf("작성자 : %s , 내용 : %s \n", post.writer, post.content);
+            }else if (ch==2) {
+                for (int i = 0; i < pArry.length; i++) {
+                    if (pArry[i] != null) {
+                        System.out.printf("작성자: %s, 내용: %s\n", pArry[i].writer, pArry[i].content);
                     }
                 }
             }
-        }
+        }  
+        
+
     }
 }
 
-class Post {
-
-    String content;
-    String writer;
+class Post{
+    String content; String writer;
 
     public Post() {}
 
@@ -64,5 +54,4 @@ class Post {
         this.content = content;
         this.writer = writer;
     }
-}
 }
