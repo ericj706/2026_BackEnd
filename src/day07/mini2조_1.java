fb79420ebdf080e77f01882bb182cc013a3f9b3e
Eric <ericj6215@gmail.com>
2026-08-06 16:49:18 +0900

    day07_17:00

---
+++ src/day07/mini2조_1.java
@@ -0,0 +1,44 @@
+package day07;
+
+// 의류카테고리
+class CATEGORY {
+    int CATEGORYID; //PK
+    String CATEGORYname;
+}
+// 의류table
+class CLOTHES {
+    int CLOTHESID;  // PK
+    String CLOTHESname;
+    int CATEGORYID; // FK
+    int MATERIALID; // FK
+    String IMGPATH;
+}
+// 소재table
+class MATERIAL{
+    int MATERIALID; //PK
+    String MATERIALname;
+    String MATERIALguide;
+}
+//건조방법
+class DRY {
+    int DRYID;  // PK
+    String DRYguide;
+    String advantage;
+    String caution;
+}
+//세탁기호
+class SYMBOl {
+    int SYMBOLID; // PK
+    String SYMBOLname;
+    String IMGPATH;
+}
+// 의류별 심볼기호
+class CLOTHESSYMBOLLIST {
+    int CLOTHESID;
+    int SYMBOLID;
+}
+//의류별 건조방법
+class CLOTHESDRYINGLIST{
+    int COLTHESID;
+    int DRYID;
+}
