6abd9921faadfd7d0cc200723e6c87fcc9f90a8f
ericj706 <ericj6215@gmail.com>
2026-08-20 11:24:19 +0900

    day15_11:30

--- src/day15/Exam1.java
+++ src/day15/Exam1.java
@@ -12,15 +12,21 @@ public class Exam1 {
                 2) 여러개 가능
                 3) 기본타입 불가능, 래퍼클래스 사용한다.
         */
-       // [1] 클래스내 객체 멤버변수 동일한 타입으로 값 대입
-       Box1 box1 = new Box1();
-       box1.content = "안녕하세요";
-
-       // [2] String 대신에 int 타입으로 값 대입?? 새로운 클래스 만든다.
-       Box2 box2 = new Box2();
-
-
-
+       // 1] 클래스내 객체 멤버변수 동일한 타입으로 값 대입
+        Box1 box1 = new Box1();
+        box1.content = "안녕하세요";
+        // 2] String 대신에 int 타입으로 값 대입???? 새로운 클래스 만든다.
+        Box2 box2 = new Box2();
+        box2.content = 10; // 동일한 멤버변수간 서로 다른 타입 불가능???
+        // 3] 해결방안? 제네릭 이용한 동일한 멤버변수간 서로 다른 타입 사용.
+        // - 사용하는 사람이 제네릭타입 정한다.
+        Box3< String > box3 = new Box3< String >(); 
+        box3.content = "안녕하세요";
+        Box3< Integer > box33 = new Box3<>(); // 생성자 뒤에 제네릭 생략 가능
+        box33.content = 10;
+        // - 제네릭 타입 대신에 Object 로 사용시에는 타입변환 필요.
+        ArrayList< String > list1 = new ArrayList<>(); // < 제네릭 > String 타입으로 배열 선언!
+        String[] list2 = new String[10];
        // [4] 2개 이상의 제네릭 타입 가능
        Box4<String, Integer> box4 = new Box4();
        box4.value1 = "안녕하세요"; box4.value2 = 10;
@@ -29,8 +35,8 @@ public class Exam1 {
        box44.value1 = "안녕하세요"; box44.value2 = new ArrayList<Integer>();
        // **활용처: 컬렉션 프레임워크 ( ArrayList )
         // [6] 메소드 제네릭 가능
-        Box3<String> box33 = Util.boxing("사과");
-        System.out.println(box33.content);  // 사과
+        Box3<String> box333 = Util.boxing("사과");
+        System.out.println(box333.content);  // 사과
         // [7] 상속관계 제약, < 타입 extends 상위타입 > 
         // Box5<String> box5 = new Box5(); // String타입은 Number타입의 자식이 아니라 불가능
         Box5<Integer> box5 = new Box5();   // Integer타입은 Number타입의 자식이라 가능<오버라이딩>
@@ -39,10 +45,9 @@ public class Exam1 {
     }//me
 }//ce
 
-class Box1{String content;} // 박스1 클래스내 문자열 content 대입가능.
-class Box2{int content;}
-class Box3<제네릭타입>{제네릭타입 content;}
-
+class Box1{ String content; } // 박스1 클래스내 문자열 content 대입 가능.
+class Box2{ int content; }    // 박스2 클래스내 정수형 content 대입 가능.
+class Box3< 제네릭타입 >{ 제네릭타입 content; } // < > 이용한 미지수 타입 지정
 class Box4 < T, E >{ T value1; E value2;}
 class Util{
     public static <T> Box3<T> boxing(T 매개변수){   // 메소드의 타입들을 제네릭 타입 가능
