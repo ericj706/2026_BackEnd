1077fa7d630e9a7da82c63d544787beda7edb185
ericj706 <ericj6215@gmail.com>
2026-08-21 10:53:22 +0900

    day16_11:00

---
+++ src/day16/Exam2.java
@@ -0,0 +1,12 @@
+package day16;
+
+public class Exam2 {
+    public static void main(String[] args) {
+        
+        /*  스택(Stack): 후입선출(LIFO: 가장마지막에 삽입된 자료가 가장 먼저 삭제된다.)
+            - 예) 브라우저(뒤로가기), ctrl+z(실행취소)등등
+            - Stack 클래스 이용한 수현, push 삽입, pop출력
+            
+        */
+    }
+}
